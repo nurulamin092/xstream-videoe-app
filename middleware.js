5ab9e832610ed1ad9df89881ef8c122438e32b77
nurulamin092 <mnurulamin092@gmial.com>
2024-12-07 17:55:52 +0600

    middleware added

---
+++ middleware.js
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+const { match } = require("@formatjs/intl-localematcher")
+import Negotiator from 'negotiator'
+
+let defaultLocale = 'en'
+let locales = ['bn', 'en']
+
+function getLocale(request) {
+    const acceptedLanguage = request.headers.get('accept-language') ?? undefined
+    const headers = { 'accept-language': acceptedLanguage }
+    let languages = new Negotiator({ headers }).languages()
+    return match(languages, locales, defaultLocale)
+}
+
+export function middleware(request) {
+    const pathname = request.nextUrl.pathname
+    const pathnameIsMissingLocale = locales.every(
+        (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+    )
+
+    if (pathnameIsMissingLocale) {
+        const locale = getLocale(request)
+        return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
+
+    }
+}
+
+export const config = {
+    matcher: [
+        // Skip all internal paths (_next, assets, api)
+        '/((?!api|assets|.*\\..*|_next).*)',
+        // Optional: only run on root (/) URL
+        // '/'
+    ],
+}
